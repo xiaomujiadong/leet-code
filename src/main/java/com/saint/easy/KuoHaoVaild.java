618d1d069f0d58c9486f2318b11b76cf8da85192
cwd曹文栋 <wdcao@ctrip.com>
2018-05-07 22:53:51 +0800

    add kuohao

---
+++ src/main/java/com/saint/easy/KuoHaoVaild.java
@@ -0,0 +1,34 @@
+package com.saint.easy;
+
+import java.util.Stack;
+
+public class KuoHaoVaild {
+    public static boolean isValid(String s){
+        if(s == null || s.equals("")){
+            return true;
+        }
+
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<Character>();
+
+        for(char c : chars){
+            if(c == '{' || c=='[' || c=='('){
+                stack.push(c);
+            }else if(stack.isEmpty()){
+                return false;
+            }else if(c == ')' && stack.pop()!='('){
+                return false;
+            }else if(c == ']' && stack.pop()!='['){
+                return false;
+            }else if(c == '}' && stack.pop()!='{'){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args){
+        String s = "{[()]}()";
+        System.out.println("resultr: "+isValid(s));
+    }
+}
