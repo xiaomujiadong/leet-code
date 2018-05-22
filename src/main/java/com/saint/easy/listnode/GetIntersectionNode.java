248e7f4876bd928d8d0873dff20a7fff2b2063a8
cwd曹文栋 <wdcao@ctrip.com>
2018-05-22 12:53:20 +0800

    add listnode solution

--- src/main/java/com/saint/easy/listnode/GetIntersectionNode.java
+++ src/main/java/com/saint/easy/listnode/GetIntersectionNode.java
@@ -3,28 +3,63 @@ package com.saint.easy.listnode;
 import com.saint.other.ListNode;
 
 /**
- * 给定一个链表，判断链表中是否有环。
+ * 判断2个给定链表相交的点
+ *
+ * 思路：
+ *      先分别统计2个链表的长度，
+ *      因为相交，所以是尾部对齐
+ *      所以先把长的链表多余的长度去掉，
+ *
+ *      然后剩下的长度，进行同时比较
+ *
  */
 
 public class GetIntersectionNode {
+
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode left = headA;
-        ListNode right = headB;
-        while (left!=null && right!=null){
-            if(left==right){
-                return left;
-            }else if(left.next == right){
-                return right;
-            }else if(right.next == left){
-                return left;
-            }else{
-                left = left.next;
-                right = right.next;
+
+        int lena = 0;
+        int lenb = 0;
+
+        ListNode tempa = headA;
+        while(tempa!=null){
+            ++lena;
+            tempa = tempa.next;
+        }
+
+        ListNode tempb = headB;
+        while(tempb!=null){
+            ++lenb;
+            tempb = tempb.next;
+        }
+
+        tempa = headA;
+        tempb = headB;
+        if(lena>lenb){
+            while(lena!=lenb){
+                tempa = tempa.next;
+                --lena;
+            }
+        }else if(lena < lenb){
+            while(lena!=lenb){
+                tempb = tempb.next;
+                --lenb;
             }
         }
+
+        while(tempa!=null && tempb!=null){
+            if(tempa==tempb){
+                return tempa;
+            }
+            tempa = tempa.next;
+            tempb = tempb.next;
+        }
+
         return null;
     }
 
+
+    //暴力破解
     public static ListNode violent(ListNode headA, ListNode headB) {
         ListNode left = headA;
 
